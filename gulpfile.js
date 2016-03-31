36f71e79e5863dd3ab6691efcc8695ca0c2e4e42
刘志敏 <zmliu@Ctrip.com>
2016-03-31 18:44:47 +0800

    save

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,16 @@ var gulp = require('gulp');
 var jade = require('gulp-jade');
 var less = require('gulp-less');
 var sass = require('gulp-sass');
+// 引入组件
+var htmlmin = require('gulp-htmlmin'), //html压缩
+    imagemin = require('gulp-imagemin'),//图片压缩
+    pngcrush = require('imagemin-pngcrush'),
+    minifycss = require('gulp-minify-css'),//css压缩
+    //jshint = require('gulp-jshint'),//js检测
+    uglify = require('gulp-uglify'),//js压缩
+    concat = require('gulp-concat'),//文件合并
+    rename = require('gulp-rename'),//文件更名
+    notify = require('gulp-notify');//提示信息
 //var scss = require('gulp-scss-lint');
 
 var jadeFiles = [
@@ -16,6 +26,44 @@ gulp.task('jade', function(){
             .pipe(jade({petty: true}))
             .pipe(gulp.dest(jf.dest));
     });
+});
+// 压缩html
+gulp.task('html', function() {
+    return gulp.src('public/templates/*.html')
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('public/dest/templates'))
+        .pipe(notify({message: 'html task ok'}));
+});
+// 压缩图片
+gulp.task('img', function() {
+    return gulp.src('/public/src/images/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngcrush()]
+        }))
+        .pipe(gulp.dest('public/dest/images/'))
+        .pipe(notify({ message: 'img task ok' }));
+});
+// 合并、压缩、重命名css
+gulp.task('css', function() {
+    return gulp.src('public/src/stylesheets/css/*.css')
+        .pipe(concat('main.css'))
+        .pipe(gulp.dest('public/dest/css'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(minifycss())
+        .pipe(gulp.dest('public/dest/css'))
+        .pipe(notify({ message: 'css task ok' }));
+});
+// 合并、压缩js文件
+gulp.task('js', function() {
+    return gulp.src('public/src/js/app/**/*.js')
+        //.pipe(concat('all.js'))
+        //.pipe(gulp.dest('dest/js'))
+        //.pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(gulp.dest('public/dest/js/app'))
+        .pipe(notify({ message: 'js task ok' }));
 });
 // 编译less
 gulp.task('less',function(){
---
+++ public/src/js/app/index/index.js
@@ -0,0 +1,52 @@
+(function(){
+    var index = angular.module('app', []);
+    index.run(function($http){
+
+    });
+    index.controller('PollListCtrl',function($scope,$http){
+        var sc = $scope;
+        $scope.polls = 343335;
+        $scope.result = "";
+        $scope.fn = function(num){
+            $scope.result = "电脑";
+        }
+        var init = ({
+            init : function(){
+                this.getKnowledge();
+                this.getOpera();
+            },
+            // 热门知识推荐
+            getKnowledge : function(){
+                $http({
+                    url : '/getHotKnowledge',
+                    method : 'POST',
+                    data : {
+                        aa : 1
+                    }
+                }).success(function(result){
+                    setTimeout(function () {
+                        $scope.$apply(function () {
+                            $scope.dataList = result.result;
+                        });
+                    }, 0); 
+                })
+            },
+            getOpera : function(){
+                $http({
+                    url : '/getOpera',
+                    method : 'POST',
+                    data : {
+                        aa : 1
+                    }
+                }).success(function(result){
+                    setTimeout(function () {
+                        $scope.$apply(function () {
+                            $scope.operaList = result.result;
+                        });
+                    }, 0);
+                })
+            }
+        }).init();
+        $scope.show = true;
+    });
+})();
