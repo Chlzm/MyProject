1bdb155732b6d6c75d81b75801dbaa0843e3cece
cyh5201314 <236882283@qq.com>
2016-04-01 16:21:47 +0800

    save

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
 var less = require('gulp-less');
-var sass = require('gulp-sass');
 // 引入组件
 var htmlmin = require('gulp-htmlmin'), //html压缩
     imagemin = require('gulp-imagemin'),//图片压缩
@@ -13,11 +12,6 @@ var htmlmin = require('gulp-htmlmin'), //html压缩
     rename = require('gulp-rename'),//文件更名
     notify = require('gulp-notify');//提示信息
 //var scss = require('gulp-scss-lint');
-
-var jadeFiles = [
-    {src: './public/partials/list.jade', dest: './public/partials/'},
-    {src: './public/templates/index.jade', dest: './public/templates/'}
-];
 
 gulp.task('jade', function(){
     jadeFiles.forEach(function(jf){
@@ -35,25 +29,25 @@ gulp.task('html', function() {
         .pipe(notify({message: 'html task ok'}));
 });
 // 压缩图片
-gulp.task('img', function() {
-    return gulp.src('/public/src/images/*')
+/*gulp.task('image', function() {
+    return gulp.src('public/src/images/!*.jpg')
         .pipe(imagemin({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
             use: [pngcrush()]
         }))
-        .pipe(gulp.dest('public/dest/images/'))
-        .pipe(notify({ message: 'img task ok' }));
-});
+        .pipe(gulp.dest('public/dest/images'));
+        //.pipe(notify({ message: 'img task ok' }));
+});*/
 // 合并、压缩、重命名css
 gulp.task('css', function() {
     return gulp.src('public/src/stylesheets/css/*.css')
-        .pipe(concat('main.css'))
-        .pipe(gulp.dest('public/dest/css'))
+        .pipe(concat('all.css'))
+        .pipe(gulp.dest('public/dest/stylesheets/css'))
         .pipe(rename({ suffix: '.min' }))
         .pipe(minifycss())
-        .pipe(gulp.dest('public/dest/css'))
-        .pipe(notify({ message: 'css task ok' }));
+        .pipe(gulp.dest('public/dest/stylesheets/css'))
+        //.pipe(notify({ message: 'css task ok' }));
 });
 // 合并、压缩js文件
 gulp.task('js', function() {
@@ -63,20 +57,25 @@ gulp.task('js', function() {
         //.pipe(rename({ suffix: '.min' }))
         .pipe(uglify())
         .pipe(gulp.dest('public/dest/js/app'))
-        .pipe(notify({ message: 'js task ok' }));
+        //.pipe(notify({ message: 'js task ok' }));
 });
+gulp.task('libjs',function(){
+    return gulp.src('public/src/js/lib/**.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('public/dest/js/lib'))
+})
 // 编译less
 gulp.task('less',function(){
-	gulp.src('public/stylesheets/less/*.less')
+	gulp.src('public/src/stylesheets/less/*.less')
 	.pipe(less())
-	.pipe(gulp.dest('public/stylesheets/css'))
+	.pipe(gulp.dest('public/dest/stylesheets/css'))
 });
 //编译sass
-gulp.task('sass',function(){
-	gulp.src('public/stylesheets/sass/*.sass')
+/*gulp.task('sass',function(){
+	gulp.src('public/stylesheets/sass/!*.sass')
 	.pipe(sass())
 	.pipe(gulp.dest('public/stylesheets/css'))
-});
+});*/
 // 编译scss
 /*gulp.task('scss',function(){
 	return gulp.src('public/stylesheets*//*.scss')
@@ -87,8 +86,9 @@ gulp.task('sass',function(){
 });*/
 gulp.task('watch', function(){
     //gulp.watch('./public/partials/*.jade',['jade']);
-	gulp.watch('./public/stylesheets/**/*.less',['less']);
-	gulp.watch('./public/stylesheets/**/*.sass',['sass']);
+	gulp.watch('./public/src/stylesheets/less/*.less',['less']);
+	gulp.watch('./public/src/js/app/**/*.js',['js']);
+	//gulp.watch('./public/stylesheets/**/*.sass',['sass']);
 	//gulp.watch('./public/stylesheets/*.scss',['scss']);
 });
 gulp.task('default',['watch'])
