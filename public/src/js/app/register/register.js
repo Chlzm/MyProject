d59f48e8a299200f104a78c5c4cec8d66c4496db
cyh5201314 <236882283@qq.com>
2016-04-03 06:03:43 +0800

    save

--- public/src/js/app/register/register.js
+++ public/src/js/app/register/register.js
@@ -1,9 +1,10 @@
 (function(){
     var index = angular.module('register', []);
     index.controller('Register',function($scope,$http){
-        $scope.userName = "222";           // 用户名
+        $scope.userName = "";      // 用户名
         $scope.password = "";      // 密码
         $scope.nickName = "";      // 昵称
+        $scope.$watch()
         var init = ({
             init : function(){
 
