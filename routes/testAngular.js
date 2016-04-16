a08e95780134ee76e7306ccfa188b2789601519e
cyh5201314 <236882283@qq.com>
2016-04-16 21:41:54 +0800

    save

--- routes/testAngular.js
+++ routes/testAngular.js
@@ -1,8 +1,8 @@
 module.exports = function(o){
     // 后台接口
     o.app.post('/getAngularInfo',function(req,res){
-        var result = req.body.name ? {name:req.body.name} : {};
-        o.db.info2.find(result,function(err,doc){
+        // var result = req.body.name ? {name:req.body.name} : {};
+        o.db.info2.find({},function(err,doc){
             if(doc.length){
                 res.json({
                     errorNumber : 1,
